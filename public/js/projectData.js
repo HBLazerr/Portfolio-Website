5509f69661b7dba9d64f3636894b1d5cad5962fa
hblazerr <signedbylazerr@gmail.com>
2025-03-25 18:44:51 -0700

    changed weatherDAN live link

--- public/js/projectData.js
+++ public/js/projectData.js
@@ -8,7 +8,7 @@ export const projectData = {
     year: "2024",
     description:
       "Anim sunt mollit minim mollit voluptate nulla non. Esse consectetur quis cupidatat ea ipsum laboris tempor ullamco consectetur amet nisi est eu reprehenderit.",
-    url: "https://youtu.be/PVRaxcB78IE",
+    url: "https://weather-dan.up.railway.app/",
     image: "../media/WeatherDAN.png",
     gradient:
       "linear-gradient(160deg, rgba(0, 89, 39, 1) 0%, rgba(35, 35, 35, 1) 40%, rgba(0, 0, 0, 1) 100%)",
