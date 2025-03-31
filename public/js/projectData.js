440e2524cd241a02cc4f63956940f1fe8c871efe
hblazerr <signedbylazerr@gmail.com>
2025-03-31 09:48:45 -0700

    added all new project screenshots and more project content to projectData.js

--- public/js/projectData.js
+++ public/js/projectData.js
@@ -26,17 +26,17 @@ export const projectData = {
     tools: "TKINTER, SPOTIPY",
     duration: "24 HRS",
     year: "2024",
-    description: "Custom desktop Spotify player that uses OAuth2 authentication to play tracks, view users top/recent songs, and control playback with a GUi built in tkinter.",
+    description: "Custom desktop Spotify player that uses OAuth2 authentication and Spotify API to play tracks, view users top/recent songs, and control playback with a GUi built with tkinter.",
     url: "https://youtu.be/clU2UqZ8BuU?si=FBGhrCNIEr_Dbyal",
     image: "../media/lazerrs-player.png",
-    screenshots: ["../media/lazerrs-player.png", "../media/lazerrs-player.png", "../media/lazerrs-player.png", "../media/lazerrs-player.png", "../media/lazerrs-player.png"], // insert key UI shots or GIFs here
+    screenshots: ["../media/lazerrs-player.png", "../media/lazerrs-player-screenshot1.png", "../media/lazerrs-player-screenshot2.png", "../media/lazerrs-player-screenshot3.png"],
     role: "Solo Developer",
     source: "https://github.com/HBLazerr/AI-Teamup", // private repo
-    extra: "",  // planning doc, figma, notion etc if any
+    extra: "https://github.com/HBLazerr/AI-Teamup",  // planning doc, figma, notion etc if any
     gradient: "linear-gradient(90deg, rgba(172, 60, 252, 1) 0%, rgba(51, 0, 102, 1) 50%, rgb(15, 15, 15) 100%)",
     mainColor: "rgb(172, 60, 252)",
     dimmedColor: "rgba(172, 60, 252, .6)",
-    objectives: ["obj1", "obj2", "obj3", "obj5"]
+    objectives: ["obj2", "obj3", "obj5"]
   },
 
   section3: {
@@ -50,9 +50,9 @@ export const projectData = {
     url: "https://weather-dan.up.railway.app/",
     image: "../media/WeatherDAN-gif.gif",
     screenshots: ["../media/WeatherDAN.png", "../media/WeatherDAN-screenshot1.png", "../media/WeatherDAN-screenshot2.png"],
-    role: "",
+    role: "Full Stack Developer",
     source: "https://github.com/HBLazerr/CSC256/tree/main/Weather%20App", // private repo
-    extra: "",
+    extra: "https://github.com/HBLazerr/CSC256/tree/main/Weather%20App",
     gradient:
       "linear-gradient(160deg, rgba(0, 89, 39, 1) 0%, rgba(35, 35, 35, 1) 40%, rgba(0, 0, 0, 1) 100%)",
     mainColor: "rgb(8, 246, 111)",
@@ -69,53 +69,72 @@ export const projectData = {
     description: "A mobile self-improvement app built from scratch to support individuals through heartbreak using personalized tools and mental wellness features.",
     url: "https://youtu.be/a7s_K5p8KdI",
     image: "../media/HeartsBreak-screenshot2.png",
-    screenshots: ["../media/HeartsBreak-screenshot1.png", "../media/HeartsBreak-screenshot2.png", "../media/HeartsBreak-screenshot3.png"],
+    screenshots: ["../media/HeartsBreak-screenshot1.png", "../media/HeartsBreak-screenshot2.png", "../media/HeartsBreak-screenshot3.png", "../media/HB-problem-statement.png", "../media/HeartsBreak-screenshot4.jpg", "../media/HeartsBreak-screenshot5.jpg", "../media/HeartsBreak-screenshot6.jpg"],
     role: "Developer and Designer",
     source: "https://snack.expo.dev/@lazerr/hearts-break?platform=ios",
     extra: "https://docs.google.com/document/d/1PVig2AcxE8CG4pxo-hu1PkPI9GV2Fgap-mNkQmoeHeI/edit?usp=sharing",
     gradient: "linear-gradient(45deg, rgba(5, 15, 20, 1) 0%, rgba(24, 45, 41, 1) 50%, rgba(60, 76, 84, 1) 100%)",
     mainColor: "rgb(228, 40, 69)",
     dimmedColor: "rgba(228, 40, 69, .6)",
-    objectives: ["obj1", "obj4"]
+    objectives: ["obj1", "obj4", "obj5"]
   },
 
   section5: {
+    name: "Alien Attack Game",
+    type: "SOFTWARE",
+    tools: "C++, VISUAL STUDIO",
+    duration: "15 HRS",
+    year: "2022",
+    description: "C++ choice-based console game where you escape or fight aliens. Shows input/output, conditionals, loops, classes, and headers.",
+    url: "https://youtu.be/fw1ZUdluxDI?si=Fd_FB-ZNdn4WhDOj",
+    image: "../media/AlienAttack-gif.gif",
+    screenshots: ["../media/AlienAttack-screenshot1.png", "../media/AlienAttack-screenshot2.png", "../media/AlienAttack-screenshot3.png"],
+    role: "Solo Developer",
+    source: "https://github.com/HBLazerr/Alien-Attack",
+    extra: "https://github.com/HBLazerr/Alien-Attack",
+    gradient: "linear-gradient(160deg, rgb(28, 85, 23) 0%, rgb(12, 12, 12) 50%, rgb(2, 12, 1) 100%)",
+    mainColor: "rgb(51, 184, 39)",
+    dimmedColor: "rgba(51, 184, 39, .6)",
+    objectives: ["obj2"]
+  },
+
+  section6: {
     name: "Movie LSTER",
     type: "WEB",
     tools: "HTML/CSS/JS",
     duration: "6 HRS",
     year: "2024",
     description:
-      "Occaecat qui reprehenderit fugiat ea ea voluptate. Pariatur non occaecat incididunt exercitation ut culpa deserunt mollit magna deserunt consectetur elit et.",
+      "A movie list web app that lets users add, sort, and clear favorite movies. Demonstrates arrays, DOM manipulation, and list-based sorting.",
     url: "https://hblazerr.github.io/CSC256/Movie%20List/index.html",
-    image: "../media/MovieLSTER.png",
-    screenshots: [],
-    role: "",
-    source: "",
-    extra: "",
+    image: "../media/MovieLSTER-gif.gif",
+    screenshots: ["../media/MovieLSTER.png", "../media/MovieLSTER-screenshot1.png", "../media/MovieLSTER-screenshot2.png", "../media/MovieLSTER-screenshot3.png"],
+    role: "Solo Developer",
+    source: "https://github.com/HBLazerr/CSC256/tree/main/Movie%20List",
+    extra: "https://youtu.be/axglgMp51Tg",
     gradient:
       "linear-gradient(180deg, rgba(76, 13, 13, 1) 0%, rgba(43, 46, 53, 1) 64%, rgba(193, 195, 198, 1) 100%)",
     mainColor: "rgb(171, 173, 177)",
     dimmedColor: "rgba(171, 173, 177, .6)",
     objectives: ["obj6"]
   },
-  section6: {
-    name: "",
-    type: "",
-    tools: "",
-    duration: "",
-    year: "",
-    description: "",
-    url: "",
-    image: "",
-    screenshots: [],
-    role: "",
-    source: "",
-    extra: "",
-    gradient: "",
-    mainColor: "",
-    dimmedColor: "",
-    objectives: []
-  }
+  section7: {
+    name: "Pokémon Data Analysis",
+    type: "DATA",
+    tools: "PYTHON, PANDAS",
+    duration: "14 HRS",
+    year: "2024",
+    description: "Explored a Kaggle Pokémon dataset of 802 entries to analyze stats like speed, type, and abilities, aiming to find patterns that predict legendary status.",
+    url: "https://colab.research.google.com/drive/186He68kiXGm9l7ZlUo5bJjfRFVrOGfav?usp=sharing",
+    image: "../media/pokemon-data-screenshot5.png",
+    screenshots: ["../media/pokemon-data-screenshot1.png", "../media/pokemon-data-screenshot2.png", "../media/pokemon-data-screenshot3.png", "../media/pokemon-data-screenshot4.png", "../media/pokemon-data-screenshot5.png", "../media/pokemon-data-screenshot6.png", "../media/pokemon-data-screenshot7.png"],
+    role: "Data Analyst",
+    source: "https://github.com/HBLazerr/pokemon-data-analysis",
+    extra: "https://www.kaggle.com/datasets/rounakbanik/pokemon",
+    gradient: "linear-gradient(90deg, rgba(243, 101, 42, 1) 0%, rgba(255, 123, 0, 1) 50%, rgba(255, 63, 0, 1) 100%)",
+    mainColor: "rgb(243, 101, 42)",
+    dimmedColor: "rgba(243, 101, 42, .6)",
+    objectives: ["obj6"]
+  },
 };
 
